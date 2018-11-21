7c573d17c5901ad18df98c241fbd04916a553eea
Eric Zhang <ekzhang1@gmail.com>
2018-11-20 18:11:58 -0600

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f origin master:gh-pages
+git push -f https://github.com/ekzhang/game-of-life.git master:gh-pages
 
 cd -
