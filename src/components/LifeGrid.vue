a9f7253ec26032cc8cd2a890844f98f79be99704
Eric Zhang <ekzhang1@gmail.com>
2018-11-22 11:45:02 -0600

    Smooth panning

--- src/components/LifeGrid.vue
+++ src/components/LifeGrid.vue
@@ -19,22 +19,34 @@ export default {
     return {
       width: null,
       height: null,
-      row0: 0,
-      col0: 0
+      rowx: 0,
+      colx: 0
     }
   },
   computed: {
     rows() {
-      return Math.ceil(this.height / (this.size + 1))
+      return Math.ceil(this.height / (this.size + 1)) + 1
     },
     cols() {
-      return Math.ceil(this.width / (this.size + 1))
+      return Math.ceil(this.width / (this.size + 1)) + 1
+    },
+    row0() {
+      return Math.floor(this.rowx)
+    },
+    col0() {
+      return Math.floor(this.colx)
+    },
+    rowOffset() {
+      return Math.round((this.size + 1) * (this.rowx - this.row0))
+    },
+    colOffset() {
+      return Math.round((this.size + 1) * (this.colx - this.col0))
     },
     all() {
       return {
         cells: this.cells,
-        row0: this.row0,
-        col0: this.col0,
+        rowx: this.rowx,
+        colx: this.colx,
         size: this.size,
         width: this.width,
         height: this.height
@@ -43,39 +55,45 @@ export default {
   },
   methods: {
     handleTap(evt) {
-      const r = this.row0 + Math.floor(evt.center.y / (this.size + 1))
-      const c = this.col0 + Math.floor(evt.center.x / (this.size + 1))
+      const r = Math.floor(this.rowx + evt.center.y / (this.size + 1))
+      const c = Math.floor(this.colx + evt.center.x / (this.size + 1))
       this.$emit('toggle', [r, c])
     },
     handlePanStart() {
-      this.oldRow0 = this.row0
-      this.oldCol0 = this.col0
+      this.oldRowx = this.rowx
+      this.oldColx = this.colx
     },
     handlePan(evt) {
-      this.row0 = this.oldRow0 - Math.round(evt.deltaY / (this.size + 1))
-      this.col0 = this.oldCol0 - Math.round(evt.deltaX / (this.size + 1))
+      this.rowx = this.oldRowx - evt.deltaY / (this.size + 1)
+      this.colx = this.oldColx - evt.deltaX / (this.size + 1)
     },
     handleResize() {
       this.width = this.$refs.canvas.width = document.body.clientWidth
       this.height = this.$refs.canvas.height = document.body.clientHeight
     },
     center() {
-      this.row0 = Math.floor(-this.rows / 2)
-      this.col0 = Math.floor(-this.cols / 2)
+      this.rowx = -this.height / (this.size + 1) / 2
+      this.colx = -this.width / (this.size + 1) / 2
     }
   },
   watch: {
+    size(newSize, oldSize) {
+      this.rowx += this.height / 2 * (1 / (oldSize + 1) - 1 / (newSize + 1))
+      this.colx += this.width / 2 * (1 / (oldSize + 1) - 1 / (newSize + 1))
+    },
     all() {
       this.ctx.clearRect(0, 0, this.width, this.height)
 
       this.ctx.beginPath()
       for (let r = 0; r < this.rows; r++) {
-        this.ctx.moveTo(0, (this.size + 1) * r + this.size + 0.5)
-        this.ctx.lineTo(this.width, (this.size + 1) * r + this.size + 0.5)
+        const y = (this.size + 1) * r - this.rowOffset + this.size + 0.5
+        this.ctx.moveTo(0, y)
+        this.ctx.lineTo(this.width, y)
       }
       for (let c = 0; c < this.cols; c++) {
-        this.ctx.moveTo((this.size + 1) * c + this.size + 0.5, 0)
-        this.ctx.lineTo((this.size + 1) * c + this.size + 0.5, this.height)
+        const x = (this.size + 1) * c - this.colOffset + this.size + 0.5
+        this.ctx.moveTo(x, 0)
+        this.ctx.lineTo(x, this.height)
       }
       this.ctx.strokeStyle = '#D3D3D3'
       this.ctx.stroke()
@@ -85,8 +103,8 @@ export default {
         if (r >= this.row0 && r < this.row0 + this.rows
             && c >= this.col0 && c < this.col0 + this.cols) {
           this.ctx.fillRect(
-            (this.size + 1) * (c - this.col0),
-            (this.size + 1) * (r - this.row0),
+            (this.size + 1) * (c - this.col0) - this.colOffset,
+            (this.size + 1) * (r - this.row0) - this.rowOffset,
             this.size,
             this.size)
         }
